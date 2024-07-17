4b6ee566881e3ec70337883312cb8de3e4b9f1cd
Antonio Marques <antonio@quimbik.com>
2024-07-17 18:05:44 -0300

    fix typescript issues

--- src/app/settings/layout.tsx
+++ src/app/settings/layout.tsx
@@ -1,7 +1,6 @@
-import { Component } from "react"
 import React from 'react'
 
-export default function Layout({ children }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
         <h1 className='text-2xl font-bold'>Layout Settings Wrapper</h1>
